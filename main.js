33e0f27b7f3575618a02864cca8ff8b7b8333c04
cj.park <sisyphus9402@gmail.com>
2023-06-16 23:59:08 +0900

    Add: example codes

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const { spawn } = require('child_process');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+
+  win.loadFile('index.html')
+
+  // Path to Python venv
+  const venvPath = path.join(__dirname, '..', 'server', '.venv', 'bin', 'python');
+
+  // Path to Flask application
+  const flaskAppPath = path.join(__dirname, '..', 'server', 'app.py');
+
+  // Start Flask server
+  const flaskServer = spawn(venvPath, [flaskAppPath]);
+
+  flaskServer.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  flaskServer.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  flaskServer.on('close', (code) => {
+    console.log(`Flask server exited with code ${code}`);
+  });
+}
+
+app.whenReady().then(createWindow)
